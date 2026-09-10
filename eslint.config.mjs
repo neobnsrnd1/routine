064ae6d1eec176cd7dea2831fa9a0d0a06236864
neobnsrnd1 <neobns.rnd1@gmail.com>
2026-09-10 13:42:17 +0900

    refactor(status) : eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([...nextVitals, ...nextTs]);
+const eslintConfig = defineConfig([
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+  ...nextVitals,
+  ...nextTs,
+]);
 
 export default eslintConfig;
